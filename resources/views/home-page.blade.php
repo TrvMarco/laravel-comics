f392aca00077f102e29a947601eea696d36a3111
TrvMarco <trevisanello.marco@gmail.com>
2022-07-07 18:50:15 +0200

    Aggiunto stampa card comics

--- resources/views/home-page.blade.php
+++ resources/views/home-page.blade.php
@@ -5,5 +5,20 @@
 @endsection
 
 @section('page-content')
-    @dump($comics)
+    <main>
+        <div class="container">
+            <div class="row gy-5">
+                @foreach ($comics as $comic)
+                <div class="col-2">
+                    <div class="comic-card text-uppercase">
+                        <div class="comic-img pb-3">
+                            <img src="{{$comic['thumb']}}" alt="">
+                        </div>
+                        <h6>{{$comic['series']}}</h6>
+                    </div>
+                </div>
+                @endforeach
+            </div>
+        </div>
+    </main>
 @endsection
