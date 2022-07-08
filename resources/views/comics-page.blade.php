4d8b2c3c27c0407ac19ca0156e5f1067403b3ce0
TrvMarco <trevisanello.marco@gmail.com>
2022-07-08 17:29:08 +0200

    Creato bozza layout con stampa fumetto

--- resources/views/comics-page.blade.php
+++ resources/views/comics-page.blade.php
@@ -22,12 +22,76 @@
                     <p>{{$singleComic['description']}}</p>
                 </div>
                 <div class="col-4">
-                    <h2>adv-placeholder</h2>
+                    <span>aaa</span>
+                    <img src="{{ asset('img/adv.jpg') }}" alt="Adv">
                 </div>
             </div>
         </div>
         <section class="comic-specs">
-            
+            <div class="container">
+                <div class="row">
+                    <div class="col-6">
+                        <h4>Talent</h4>
+                        <hr>
+                        <div class="row">
+                            <div class="col-4">
+                                Art by:
+                            </div>
+                            <div class="col-8">
+                                <p>
+                                    @foreach ($singleComic['artists'] as $artist)
+                                        {{$artist}},
+                                    @endforeach
+                                </p>
+                            </div>
+                        </div>
+                        <hr>
+                        <div class="row">
+                            <div class="col-4">
+                                Written by:
+                            </div>
+                            <div class="col-8">
+                                <p>
+                                    @foreach ($singleComic['writers'] as $writer)
+                                    {{$writer}},
+                                    @endforeach
+                                </p>
+                            </div>
+                        </div>
+                        <hr>
+                    </div>
+                    <div class="col-6">
+                        <h4>Specs</h4>
+                        <hr>
+                        <div class="row">
+                            <div class="col-4">
+                                Series:
+                            </div>
+                            <div class="col-8 text-uppercase">
+                                {{$singleComic['series']}}
+                            </div>
+                        </div>
+                        <hr>
+                        <div class="row">
+                            <div class="col-4">
+                                U.S Price:
+                            </div>
+                            <div class="col-8 text-uppercase">
+                                {{$singleComic['price']}}
+                            </div>
+                        </div>
+                        <hr>
+                        <div class="row">
+                            <div class="col-4">
+                                On Sale Date:
+                            </div>
+                            <div class="col-8 text-uppercase">
+                                {{$singleComic['sale_date']}}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </section>
     </main>
 @endsection
