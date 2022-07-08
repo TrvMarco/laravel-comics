9cdfc106da439db52eaf9778bd9897f9ca3a348b
TrvMarco <trevisanello.marco@gmail.com>
2022-07-08 14:11:05 +0200

    Creato componente card per home page

---
+++ resources/views/components/card.blade.php
@@ -0,0 +1,6 @@
+<div class="comic-card text-uppercase">
+    <div class="comic-img pb-3">
+        <img src="{{$image}}" alt="{{$title}}">
+    </div>
+    <h6>{{$title}}</h6>
+</div>
--- resources/views/home-page.blade.php
+++ resources/views/home-page.blade.php
@@ -12,12 +12,7 @@
                 <div class="row gy-5">
                     @foreach ($comics as $comic)
                     <div class="col-2">
-                        <div class="comic-card text-uppercase">
-                            <div class="comic-img pb-3">
-                                <img src="{{$comic['thumb']}}" alt="">
-                            </div>
-                            <h6>{{$comic['series']}}</h6>
-                        </div>
+                        <x-card :image="$comic['thumb']" :title="$comic['series']"/>
                     </div>
                     @endforeach
                 </div>
